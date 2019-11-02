9f58b35d6c06c0f25d528563b2cabd8d829c0722
ztk <ztk@iastate.edu>
2019-11-02 16:09:17 -0500

    worked on moving down the angles to between 60 and 30 degrees

--- servosWithOffset/servosWithOffset.ino
+++ servosWithOffset/servosWithOffset.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
-const int maxAngle = 45;
+const int maxAngle = 20;
+const float startAngle = 90;
 Servo servo1, servo2, servo3, servo4, servo5, servo6;  // create servo object to control a servo
 
 const int servoPin1 = 2;
@@ -9,7 +10,7 @@ const int servoPin4 = 5;
 const int servoPin5 = 6;
 const int servoPin6 = 7;
 int pos = 0;    
-int offSet = 5; //the degree offset on each of the servos
+float offSet = 30 * M_PI / 180; //the degree offset on each of the servos
 int i;
 void setup() {
   Serial.begin(9600);
@@ -22,18 +23,18 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 360; pos += 2) {
-    float val = maxAngle * sin(pos * (2* M_PI ) / 360) + maxAngle * 2;
+  for (pos = 0; pos <= 360; pos += 3) {
+    float val = maxAngle * sin(pos * (2* M_PI ) / 360) + startAngle;
     Serial.println(val);
     //servo 1 is 5 degrees "ahead" of servo 2, which is 5 degres "ahead" of servo 3 
     servo1.write(val);
-    servo4.write(val);//TODO: calculate mirrored servo
-    val =  maxAngle * sin(pos * (2* M_PI ) / 360 + offSet) + maxAngle;
+    servo4.write(180 - val);//TODO: calculate mirrored servo
+    val =  maxAngle * sin(pos * (2* M_PI ) / 360 + offSet) + startAngle;
     servo2.write(val);
-    servo5.write(val);//TODO: calculate mirrored servo
-    val =  maxAngle * sin(pos * (2* M_PI ) / 360 + offSet * 2) + maxAngle;
+    servo5.write(180 - val);//TODO: calculate mirrored servo
+    val =  maxAngle * sin(pos * (2* M_PI ) / 360 + offSet * 2) + startAngle;
     servo3.write(val);
-    servo6.write(val); //TODO: calculate mirrored servo
-    delay(10);             
+    servo6.write(180 - val); //TODO: calculate mirrored servo
+    delay(1);             
   }
 }
